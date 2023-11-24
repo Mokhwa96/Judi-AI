75aae3280732f4bef61a99a9c2a1557172429e83
Kim Seol <rlatjf608@naver.com>
2023-11-24 17:17:45 +0900

    데이터베이스 질문 응답 번갈아가며 나오게 수정

--- hh/server.js
+++ hh/server.js
@@ -57,19 +57,37 @@ app.post('/chat', (req, res) => {
         return;
     }
     const dictionaries = [];  // 딕셔너리들을 저장할 배열
-    dictionaries.push({"role": "system", "content": "너는 법률 문제에 대해 상담을 진행해주는 변호사야. 지금 나는 너에게 법률 문제에 대해 상담을 받으러 왔고, 내가 처한 상황을 설명할거야. 너는 내가 하는 말에 공감해주면서 사실관계 파악을 위해 부족한 정보가 있다면 하나씩 친절하게 물어볼 수 있어. 사실관계 파악을 위한 충분한 정보가 모였다면, 마지막에는 파악된 정보를 요약해서 알려줘"});
+
+    let currentUser = null;
+    let currentAssistant = null;
+
     results.forEach(row => {
-        const id = row.id;
         const role = row.role;
         const text = row.text;
 
-        if (role == 'user' || role == 'assistant') {
-            dictionaries.push({
-                role: role,
-                content: text
-            });
-        } 
-            });
+        if (role === 'user') {
+            currentUser = { role: 'user', content: text };
+            if (currentAssistant !== null) {
+                dictionaries.push(currentAssistant);
+                currentAssistant = null;
+            }
+        } else if (role === 'assistant') {
+            currentAssistant = { role: 'assistant', content: text };
+            if (currentUser !== null) {
+                dictionaries.push(currentUser);
+                currentUser = null;
+            }
+        }
+    });
+
+    // 마지막 문장 추가
+    if (currentUser !== null) {
+        dictionaries.push(currentUser);
+    }
+    if (currentAssistant !== null) {
+        dictionaries.push(currentAssistant);
+    }
+
         
     console.log('딕셔너리 확인')
     console.log(dictionaries);
