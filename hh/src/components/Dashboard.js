405d004f70c76652b96aba204f0e3cf717f7832f
HyunHo <gh5768@naver.com>
2023-11-28 11:45:47 +0900

    프롬프트 수정중-

--- hh/src/components/Dashboard.js
+++ hh/src/components/Dashboard.js
@@ -136,7 +136,7 @@ function Dashboard() {
         { text: userInput, sender: "user" },
       ]);
       chatbotChat(userInput);
-      setUserInput("".trim()); // 사용자 입력을 초기화
+      setUserInput("".replace('\n', '')); // 사용자 입력을 초기화
     }
   };
 
@@ -147,16 +147,9 @@ function Dashboard() {
 
   // 메시지 배열을 기반으로 UI 렌더링
   const renderMessages = messages.map((message, index) => (
-    <div
-      key={index}
-      className={`message-container ${message.sender}-container`}
-    >
+    <div key={index} className={`message-container ${message.sender}-container`}>
       <div className={`bubble ${message.sender}`}>
-        {message.sender === "lawyer" && isLoading ? (
-          <img className="loading-chat_image" src={Loading} alt="로딩이미지" />
-        ) : (
-          message.text
-        )}
+        {message.text.split('\n').map((line, index) => <React.Fragment key={index}>{line}<br /></React.Fragment>)}
       </div>
     </div>
   ));
@@ -257,7 +250,6 @@ function Dashboard() {
                 stroke-linecap="round"
                 stroke-linejoin="round"
                 stroke-width="2"
-                className="feather feather-pie-chart"
                 viewBox="0 0 24 24"
               >
                 <defs />
@@ -294,7 +286,6 @@ function Dashboard() {
                 stroke-linecap="round"
                 stroke-linejoin="round"
                 stroke-width="2"
-                className="feather feather-settings"
                 viewBox="0 0 24 24"
               >
                 <defs />
@@ -372,6 +363,11 @@ function Dashboard() {
                 {/* 채팅 메시지를 표시하는 부분 */}
                 <div className="chat-messages">
                   {renderMessages}
+                  {isLoading ? (
+                    <div className={'bubble lawyer'}>
+                      <img className='loading-chat-image' src={Loading} alt='로딩이미지' />
+                    </div>
+                  ) : (<></>)}
                   <div ref={messagesEndRef} />{" "}
                   {/* 스크롤 조정을 위한 빈 div 추가 */}
                 </div>
